4b53c7223334aae6ca9737adbee58d439d1435f0
lucas-po <lucaschang1127@gmail.com>
2024-03-11 16:54:27 +0700

    improve stps spammer

--- spammer/v6.js
+++ spammer/v6.js
@@ -1,7 +1,7 @@
 var { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 var { cryptoWaitReady } = require('@polkadot/util-crypto');
 
-const LIMIT = 50_000;
+const LIMIT = 25000;
 
 // Main function which needs to run at start
 async function main() {
@@ -20,36 +20,47 @@ async function main() {
 		`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
 	);
 
-	let accounts = [];
-	let num_accounts = 10000;
+	let alice_accounts = [];
+	let bob_accounts = [];
 
-	for (let i = 0; i < num_accounts + 1; i += 1) {
-		let account = keyring.addFromUri(`//Alice//${i % num_accounts}`, { name: `${i} Account` });
-		let { nonce } = await api.query.system.account(
-			account.address
+	let num_accounts = 10_000;
+
+	for (let i = 0; i < num_accounts; i += 1) {
+
+		if ((10 * i) % num_accounts == 0) {
+			console.log("Accounts: ", (100 * i) / num_accounts, '%');
+		}
+
+		let alice_account = keyring.addFromUri(`//Alice//${i % num_accounts}`, { name: `Alice ${i} Account` });
+		let { nonce: alice_nonce } = await api.query.system.account(
+			alice_account.address
 		);
 
-		accounts.push({ account, nonce: nonce.toNumber() })
+		alice_accounts.push({ account: alice_account, nonce: alice_nonce.toNumber() })
+
+		let bob_account = keyring.addFromUri(`//Bob//${i % num_accounts}`, { name: `Bob ${i} Account` });
+		bob_accounts.push(bob_account)
 	}
 
 	let txs = [];
+	let oneUnit = 1_000_000_000_000;
 
 	// Create and sign transaction ahead of time
 	for (let i = 0; i < LIMIT; i += 1) {
 
-		let { account, nonce } = accounts[i % num_accounts];
+		let { account, nonce } = alice_accounts[i % num_accounts];
 
 
 		if ((10 * i) % LIMIT == 0) {
-			console.log((100 * i) / LIMIT, '%');
+			console.log("Transaction: ", (100 * i) / LIMIT, '%');
 		}
 
 		txs.push(
-			await api.tx.system.remark(i)
+			await api.tx.balances.transferKeepAlive(bob_accounts[i % num_accounts].address, oneUnit)
 				.signAsync(account, nonce)
 		);
 
-		accounts[i % num_accounts].nonce += 1;
+		alice_accounts[i % num_accounts].nonce += 1;
 	}
 
 	for (let i = 0; i < LIMIT; i++) {
